8fdf925a3cb40a6a7a72a631357eb25bfe6c26b6
Bergson Brito Barros <bergson.barros@serpro.gov.br>
2025-04-21 11:13:04 -0300

    Ajuste na chamada da API URL.

--- src/http/index.ts
+++ src/http/index.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const http = axios.create({
     //baseURL: 'http://localhost:8000/api/v2/'
-    baseURL: 'http://localhost:3500/'
+    //baseURL: 'http://localhost:3500/'
+    baseURL: process.env.API_URL || 'http://localhost:3500/',
 })
 
 export default http
