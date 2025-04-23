6418c39474f85868625fb16f9922556e978a073d
Bergson Brito Barros <bergson.barros@serpro.gov.br>
2025-04-23 09:41:43 -0300

    Ajuste na base URL.

--- src/http/index.ts
+++ src/http/index.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const http = axios.create({
     //baseURL: 'http://localhost:8000/api/v2/'
     //baseURL: 'http://localhost:3500/'
-    baseURL: process.env.API_URL || 'http://localhost:3500/',
+    baseURL: process.env.API_URL
 })
 
 export default http
